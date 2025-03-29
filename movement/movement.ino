695b82032e719e08539ff4c366738e76014a1261
Samuel <samuelchua2523@gmail.com>
2025-03-29 17:13:55 -0400

    git commit -m "Updated file from lab.
    Bug fixing and start of line tracking"

--- movement/movement.ino
+++ movement/movement.ino
@@ -1,37 +1,100 @@
 // Motor driver pins
 #define ENA 5  // Left motor speed control
 #define ENB 6  // Right motor speed control
-#define IN1 7  // Left motor direction
-#define IN2 8
-#define IN3 9  // Right motor direction
-#define IN4 11
+#define IN1 7  // Right motor forward
+#define IN2 8  // Right motor backward
+
 #define STBY 3 // Standby pin for enabling motor driver
 
+// Line sensor pins
+#define RIGHT_SENSOR A0  
+#define CENTER_SENSOR A1
+#define LEFT_SENSOR A2
+
 // Speed of the motors (0-255)
-int motor_speed = 255;
+int motor_speed = 70;
 
 void setup() {
   // Set motor control pins as outputs
   pinMode(IN1, OUTPUT);
   pinMode(IN2, OUTPUT);
-  pinMode(IN3, OUTPUT);
-  pinMode(IN4, OUTPUT);
   pinMode(ENA, OUTPUT);
   pinMode(ENB, OUTPUT);
   pinMode(STBY, OUTPUT);
 
+  // Set sensor as input
+  pinMode(RIGHT_SENSOR, INPUT);
+  pinMode(LEFT_SENSOR, INPUT);
+  pinMode(CENTER_SENSOR, INPUT);
+
   // Enable motor driver
   digitalWrite(STBY, HIGH);
+
+  // Start serial communication
+  Serial.begin(9600);
 }
 
 void loop() {
-  // Left wheels move forward
-  analogWrite(ENA, motor_speed);
-  digitalWrite(IN1, HIGH);   // HIGH = FORWARD, LOW = BACKWARD
-  digitalWrite(IN2, HIGH);   // HIGH = FORWARD, LOW = BACKWARD
+  photosensor();
+}
 
-  // Right wheels move forward
+void forward() {
+  analogWrite(ENA, motor_speed);
   analogWrite(ENB, motor_speed);
-  digitalWrite(IN3, HIGH); // HIGH = FORWARD, LOW = BACKWARD
-  digitalWrite(IN4, HIGH); // HIGH = FORWARD, LOW = BACKWARD
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+}
+
+void stop() {
+  analogWrite(ENA, 0);
+  analogWrite(ENB, 0);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, LOW);
+}
+
+void rotate_left() {
+  analogWrite(ENA, motor_speed);
+  analogWrite(ENB, motor_speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, LOW);
+}
+
+void rotate_right() {
+  analogWrite(ENA, motor_speed);
+  analogWrite(ENB, motor_speed);
+  digitalWrite(IN1, LOW);
+  digitalWrite(IN2, HIGH);
+}
+
+void turn_left() {
+  analogWrite(ENA, motor_speed - 50);
+  analogWrite(ENB, motor_speed);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+}
+
+void turn_right() {
+  analogWrite(ENA, motor_speed);
+  analogWrite(ENB, motor_speed - 50);
+  digitalWrite(IN1, HIGH);
+  digitalWrite(IN2, HIGH);
+}
+
+void photosensor() {
+  int right_value = analogRead(RIGHT_SENSOR);
+  int left_value = analogRead(LEFT_SENSOR);
+  int center_value = analogRead(CENTER_SENSOR);
+
+
+
+  // Line-following logic
+  if (center_value >= 800 && center_value <= 1000) {  
+    forward();
+  } else if (right_value >= 800 && right_value <= 1000) {  
+    turn_left();
+  } else if (left_value >= 800 && left_value <= 1000) {
+    turn_right();
+
+  }
+
 }
