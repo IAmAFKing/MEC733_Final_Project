dd6a4ff55c6720bd2827a472194f5e9a9334ccdf
Samuel <samuelchua2523@gmail.com>
2025-03-31 02:38:27 -0400

    git commit -m "Added rotation to alg
    Turn a bit if 2 sensors, rotate if 1. Rotation values to be adjusted"

--- movement/movement.ino
+++ movement/movement.ino
@@ -103,7 +103,17 @@ bool photosensor() {
 
   // Line-following logic
   if (!(left && right)) {
-    forward();
+    if (center && left) {
+      turn_left(31);
+    } else if (center && right) {
+      turn_right(31);
+    } else if (left) {
+      rotate_left();
+    } else if (right) {
+      rotate_right();
+    } else {
+      forward();
+    }
   } else {
     stop(100);
     return true;
